3be7c0d4358904ab7ae4a45c9e8495f96699d437
Nguyen Ngoc Bang <bang270197@gmail.com>
2019-10-02 11:19:09 +0700

    project-nhanhdev

---
+++ src/main/java/com/codegym/c0319h2/quanlycongty/QuanlycongtyApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.c0319h2.quanlycongty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlycongtyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanlycongtyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/c0319h2/quanlycongty/QuanlycongtyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codegym.c0319h2.quanlycongty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuanlycongtyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
